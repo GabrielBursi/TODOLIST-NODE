823496cb87b7e189515fd3e2e09ce3be72788a31
Gabriel Bursi <gabriellbursii@gmail.com>
2023-01-11 08:29:16 -0300

    GetById, Update e Delete

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -4,7 +4,7 @@ async function getAllTasks(req, res){
     try {
         TaskModel.find()
             .then((tasks) => {
-                res.render('index',{tasks})
+                res.render('index', { tasks, task: null, taskDelete: null })
             })
             .catch(err => {
                 res.render('index', {erro: err.message})
@@ -22,15 +22,67 @@ async function createTask(req, res){
     }
 
     try {
-        await TaskModel.create({task}).then(() => {
-            res.redirect('/')
-        })
+        TaskModel.create({task})
+            .then(() => {
+                res.redirect('/')
+            }).catch(error => {
+                res.render('index', { error })
+            })
     } catch (err) {
         res.render('index', { erro: err.message })
+    }
+}
+async function getById(req, res){
+    const { id } = req.params
+    const { method } = req.params
+
+    try {
+        TaskModel.findById(id)
+            .then(task => {
+                TaskModel.find()
+                    .then(tasks => {
+                        if(method == 'update'){
+                            res.render('index', { task, tasks, taskDelete: null })
+                        }else{
+                            res.render('index', { task:null, tasks, taskDelete: task })
+                        }
+                    })
+            }).catch(error => {
+                res.render('index', { error })
+            })
+    } catch (error) {
+        res.render('index', { error })
+    }
+}
+
+async function updateOne(req, res){
+    const { task } = req.body
+
+    try {
+        TaskModel.updateOne({ _id: req.params.id }, {task})
+            .then(() => {
+                res.redirect('/')
+            })
+    } catch (error) {
+        res.render('index', { error })
+    }
+}
+
+async function deleteOne(req, res){
+    const { id } = req.params
+
+    try {
+        await TaskModel.findByIdAndDelete(id)
+        res.redirect('/')
+    } catch (error) {
+        res.render('index', { error })
     }
 }
 
 export {
     getAllTasks,
     createTask,
+    getById,
+    updateOne,
+    deleteOne
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,14 @@
 import express from 'express'
 
 //!controllers
-import { createTask, getAllTasks } from '../controllers/TaskController.js'
+import { createTask, deleteOne, getAllTasks, getById, updateOne } from '../controllers/TaskController.js'
 
 const router = express.Router()
 
 router.get('/', getAllTasks)
 router.post('/create', createTask)
+router.get('/getById/:id/:method', getById)
+router.post('/update/:id', updateOne)
+router.get('/delete/:id', deleteOne)
 
 export default router
